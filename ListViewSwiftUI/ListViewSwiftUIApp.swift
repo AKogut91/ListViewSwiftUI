ef9f1a9b72c04694b4e94223c04b532def0ee591
Alex_Kogut13 <akogut@engenious.io>
2024-05-04 15:26:41 +0300

    Initial Commit

---
+++ ListViewSwiftUI/ListViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewSwiftUIApp.swift
+//  ListViewSwiftUI
+//
+//  Created by Alex Kogut on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ListViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
